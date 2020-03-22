0aafe94aa362762ed0393a7b0ddc1b38d79db212
shun-nakazawa <shun.nakazawa@voice-research.com>
2020-03-23 08:10:47 +0900

    Support for MEME Enhancement

--- src/Relation.ts
+++ src/Relation.ts
@@ -9,15 +9,54 @@ namespace JupyterNotebook {
 		/** JQueryノード */
 		$view: JQuery;
 
+		/** Cell IDから隣の関連するCellリストへの連想配列 */
+		relatedCells: { [key: string]: Cell[] };
+
 		/** 初期化 */
 		constructor(notebookLeft: Notebook, notebookRight: Notebook) {
 			this.notebookLeft = notebookLeft;
 			this.notebookRight = notebookRight;
 			this.$view = $('<div class="relation"></div>');
+			this.relatedCells = {};
+		}
+
+		/** リレーション構造を更新する */
+		updateRelation(): void {
+			this.relatedCells = {};
+			const usedRightCells: { [key: string]: boolean } = {};
+
+			for (const cellLeft of this.notebookLeft.cellList) {
+				this.relatedCells[cellLeft.id] = [];
+			}
+			for (const cellRight of this.notebookRight.cellList) {
+				this.relatedCells[cellRight.id] = [];
+			}
+
+			for (const cellLeft of this.notebookLeft.cellList) {
+				const cellRightList = this.notebookRight.getCellsByMeme(cellLeft.meme)
+					.filter(cell => !usedRightCells[cell.id]);
+				for (const cellRight of cellRightList) {
+					this.relatedCells[cellLeft.id].push(cellRight);
+					this.relatedCells[cellRight.id].push(cellLeft);
+					usedRightCells[cellRight.id] = true;
+				}
+			}
+
+			for (const cellLeft of this.notebookLeft.cellList.filter(cell => !this.relatedCells[cell.id].length)) {
+				const cellRightList = this.notebookRight.cellList
+					.filter(cell => !usedRightCells[cell.id])
+					.filter(cell => cellLeft.memeUuid === cell.memeUuid)
+					.filter(cell => cellLeft.memeBranchNumber < cell.memeBranchNumber);
+				for (const cellRight of cellRightList) {
+					this.relatedCells[cellLeft.id].push(cellRight);
+					this.relatedCells[cellRight.id].push(cellLeft);
+					usedRightCells[cellRight.id] = true;
+				}
+			}
 		}
 
 		/** 描画を更新する */
-		update(): void {
+		updateView(): void {
 			this.$view.html(this.html());
 		}
 
@@ -28,11 +67,8 @@ namespace JupyterNotebook {
 			let height = Math.max(this.notebookLeft.$view.height(), this.notebookRight.$view.height());
 			html += '<div class="relation" style="height: ' + height + 'px">';
 			html += '<svg width="50" height="' + height + '">';
-			for (let i = 0; i < this.notebookLeft.count; i++) {
-				let cellLeft = this.notebookLeft.getCellAt(i);
-				let cellRightList = this.notebookRight.getCellsByMeme(cellLeft.meme);
-				for (let j = 0;j < cellRightList.length;j ++) {
-					let cellRight = cellRightList[j];
+			for (const cellLeft of this.notebookLeft.cellList) {
+				for (const cellRight of this.relatedCells[cellLeft.id]) {
 					let y0 = cellLeft.y + offsetY;
 					let y1 = cellRight.y + offsetY;
 					html += '<path d="M 0,' + y0 + ' C 25,' + y0 + ' 25,' + y1 + ' 50,' + y1;
